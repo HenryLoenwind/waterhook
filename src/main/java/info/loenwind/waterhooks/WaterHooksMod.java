67cddfff1d636c7e5637dac845df1775ee97bc0a
HenryLoenwind <henry@loenwind.info>
2015-03-09 22:03:06 +0100

    Refactored

--- src/main/java/info/loenwind/waterhooks/WaterHooksMod.java
+++ src/main/java/info/loenwind/waterhooks/WaterHooksMod.java
@@ -10,7 +10,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 public class WaterHooksMod
 {
     public static final String MODID = "waterhooks";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -19,7 +19,7 @@ public class WaterHooksMod
     }
 
 //    @SubscribeEvent
-//    public void onWaterForming(info.loenwind.waterhooks.Hooks.WaterFormEvent event)
+//    public void onWaterForming(WaterFormEvent event)
 //    {
 //    	event.setCanceled(true);
 //    }
